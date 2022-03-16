fb446e9bfb551fdc77480db265baf6f9cbc59e84
jamal <you@example.com>
2022-03-16 16:58:57 +0700

    fix 2 middleware

--- pages/dashboard/_middleware.js
+++ pages/dashboard/_middleware.js
@@ -1,12 +1,12 @@
 // import type { NextFetchEvent, NextRequest } from 'next/server'
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function middleware(req, ev) {
-  const sessionToken = req.cookies["next-auth.session-token"];
+// export async function middleware(req, ev) {
+//   const sessionToken = req.cookies["next-auth.session-token"];
 
-  if (sessionToken === undefined) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/";
-    return NextResponse.redirect(url);
-  }
-}
+//   if (sessionToken === undefined) {
+//     const url = req.nextUrl.clone();
+//     url.pathname = "/";
+//     return NextResponse.redirect(url);
+//   }
+// }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useE } from "react";
 import CardsDashJS from "../../components/CardsDashJS";
 import LayoutDashMainJS from "../../components/LayoutDashMainJS";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Dashboard = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      return router.push("/");
+    }
+  });
 
   if (!session) return null;
 
